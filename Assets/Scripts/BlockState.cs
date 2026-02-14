aa0ac72f3faa9b3e8d17306b46849c0485b97840
Frederick_Freeze <arhipkinnikita29@gmail.com>
2026-02-14 12:35:34 +0300

    add game states

--- Assets/Scripts/BlockState.cs
+++ Assets/Scripts/BlockState.cs
@@ -8,7 +8,7 @@ public class BlockState : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        state = Random.Range(0, 3);
+        //state = Random.Range(0, 3);
         blockSR.sprite = sprites[state];
     }
 
